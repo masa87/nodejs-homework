0c2ad98c6a2d853d037b6d084b1ac3fe595f8e02
Tomasz Mazur <61616164+masa87@users.noreply.github.com>
2022-03-21 19:07:29 +0100

    joi validation added

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,5 @@
-// const { boolean } = require("joi");
 const service = require("../service");
+const { schema } = require("../utils/joiSchema.js");
 
 const get = async (req, res, next) => {
   try {
@@ -45,20 +45,30 @@ const getById = async (req, res, next) => {
 
 const addContact = async (req, res, next) => {
   const { name, email, phone } = req.body;
-  try {
-    const result = await service.createContact({ name, email, phone });
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        contacts: result,
-      },
+  const { error } = schema.validate({ name, email, phone });
+  if (error === undefined) {
+    try {
+      const result = await service.createContact({ name, email, phone });
+      res.status(201).json({
+        status: "success",
+        code: 201,
+        data: {
+          contacts: result,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  } else {
+    res.json({
+      status: "error",
+      code: 403,
+      message: error.details[0].message,
     });
-  } catch (e) {
-    console.error(e);
-    next(e);
   }
 };
+
 const updateContact = async (req, res, next) => {
   const { id } = req.params;
   const { name, email, phone } = req.body;
